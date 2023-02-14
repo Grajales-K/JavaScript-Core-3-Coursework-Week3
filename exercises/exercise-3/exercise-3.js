6550943beaf022d64136b74760a08c50f46ca946
Karla Grajales <k.grajalesluna@gmail.com>
2023-02-14 20:07:17 +0000

    exercise 3 done

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,34 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+// ============== option a =====================
+
+// let total = 0;
+// // console.log ("QTY  ITEM  TOTAL" )
+// for (let {itemName: food, quantity: num, unitPrice: price} of order){
+//   console.log (`${num}  ${food} ${price * num }`)
+//   // total = price * num + total;
+//  }
+//  console.log(` \n Total: ${total}`)
+
+
+
+// ===================== option b ================
+
+
+const FinalBill = (orders) => {
+  console.log("QTY".padEnd(10) + "Item".padEnd(20) + "Total");// we set the indentation.
+  let orderCost = 0; 
+  orders.forEach((order) => {
+    const { itemName, quantity, unitPrice } = order;
+    const total = quantity * unitPrice;
+    orderCost += total;
+    console.log(
+      `${quantity.toString().padEnd(10)}${itemName.padEnd(20)}${total}`
+    );
+  });
+  console.log(`\nTotal: ${orderCost}`);
+};
+
+FinalBill(order);
